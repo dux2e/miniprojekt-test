d33f392fa557b937f880ca210b47fa3d0e040e69
Joakim <joak0451@gmail.com>
2021-02-25 16:47:20 +0100

    Create TestAddPayment.java
    Jeg pwøvede at gør noget

---
+++ src/test/TestAddPayment.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import controllayer.*;
+import modellayer.*;
+
+public class TestAddPayment {
+	private ControlPayStation ps;
+	
+	@Before
+	public void setUp() {
+		ps = new ControlPayStation();
+	}
+	
+	@After
+	public void cleanUp() {
+		System.out.println(new Object() {}.getClass().getEnclosingMethod().getName());
+		ps.setReady();
+	}
+	
+	@Test
+	public void testValidateCoind() {
+		
+	}
+	
+//	@Test
+//	public void testAddPayment() throws IllegalCoinException {
+//		System.out.println(new Object() {}.getClass().getEnclosingMethod().getName());
+//		assertEquals(
+//				ps.addPayment(5, Currency.ValidCurrency.DKK, Currency.ValidCoinType.INTEGER));
+//	}
+
+}
